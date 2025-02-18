4b62b0a6ab08204f43f87803c42069f5078f2c49
shubhamyadav3100 <shubhamyadav32100@gmail.com>
2025-02-18 14:42:28 +0530

    Added WebSocket configuration and test controller with DTOs, integrated Lombok in build.gradle

---
+++ src/main/java/com/socketserver/dto/ChatRequest.java
@@ -0,0 +1,17 @@
+package com.socketserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+	private String name;
+	private String message;
+}
---
+++ src/main/java/com/socketserver/dto/ChatResponse.java
@@ -0,0 +1,18 @@
+package com.socketserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponse {
+	private String name;
+	private String message;
+	private String timeStamp;
+}
